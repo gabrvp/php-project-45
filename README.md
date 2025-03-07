6c082f0fddee756d270f9425bdf3ff59b0ebafa6
Gabrusevich Valeriy <gabrvp@yandex.ru>
2025-03-07 22:38:56 +0700

    add CodeClimate & add PHP_CodeSniffer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/gabrvp/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/gabrvp/php-project-45/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/86ca40c90905090ecad7/maintainability)](https://codeclimate.com/github/gabrvp/php-project-45/maintainability)
--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace BrainGames\Cli;
-require_once __DIR__ . '/../vendor/autoload.php';
 
 use function cli\line;
 use function cli\prompt;
 
 function welcome()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
