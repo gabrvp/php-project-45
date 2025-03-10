caa6d6c223d764cd14732801247141d94b2ffe52
Gabrusevich Valeriy <gabrvp@yandex.ru>
2025-03-10 21:04:01 +0700

    add brain-even & add Games/Even.php

---
+++ src/Games/Even.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Cli\welcome;
+
+const MIN_VALUE = 1;
+const MAX_VALUE = 99;
+
+function playEven()
+{
+    welcome();
+    $result = 0;
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+}
