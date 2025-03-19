158fbb761f6e1204ead94c1807557ab69a7fea35
Gabrusevich Valeriy <gabrvp@yandex.ru>
2025-03-19 21:07:57 +0700

    Fixed Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,21 +7,20 @@ use function cli\line;
 
 const ROUNDS_COUNT = 3; #Кол-во раундов в каждой игре
 
-function executeGameTemplate(string $descriptionOfGame, callable $functionOfGame): void
+function executeGameTemplate(string $descriptionOfGame, callable $playGame): void
 {
     $name = welcome();
     line($descriptionOfGame);
 
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         #"Вытаскиваем" ответ пользователя и корректный ответ из функции конкретной игры
-        [$answerOfUser, $answerCorrect] = $functionOfGame();
-        if ($answerOfUser === $answerCorrect) {
-            line('Correct!');
-        } else {
+        [$answerOfUser, $answerCorrect] = $playGame();
+        if ($answerOfUser !== $answerCorrect) {
             line("'{$answerOfUser}' is wrong answer ;(. Correct answer was '{$answerCorrect}'");
             line("Let's try again, {$name}!");
             return;
         }
+        line('Correct!');
     }
     line("Congratulations, {$name}!");
     return;
